74114624e4a7c5417c39477bc27eb2cadec43620
Nikolay Kuchumov <kuchumovn@gmail.com>
2015-10-19 18:54:52 +0300

    misc

--- code/server/entry.js
+++ code/server/entry.js
@@ -17,12 +17,12 @@ var command_line_arguments = minimist(process.argv.slice(2))
 global._production_ = command_line_arguments.production
 global._development_ = command_line_arguments.development || process.env.NODE_ENV === 'development'
 
-// alternatively, if you you can skip using this and insted use this: 
-// (and webpack DefinePlugin for setting _client_ environment variable)
-// const picture = _client_ ? require('./image.png') : webpack_isomorphic_tools.require('./image.png')
-var webpack_configuration = require('../../webpack/webpack.config.js')
+var webpack_configuration = require('../../webpack/webpack.config')
 var Webpack_isomorphic_tools = require('webpack-isomorphic-tools')
-global.webpack_isomorphic_tools = new Webpack_isomorphic_tools(require('../../webpack/isomorphic.js')).development(global._development_).server(webpack_configuration.context, function()
+
+global.webpack_isomorphic_tools = new Webpack_isomorphic_tools(require('../../webpack/isomorphic.js'))
+.development(global._development_)
+.server(webpack_configuration.context, function()
 {
-	require(path.resolve(__dirname, 'main'))
+	require('./main')
 })
