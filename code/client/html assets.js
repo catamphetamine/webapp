2532b59059eb259c23e7ebd7585b877fdd423d77
Nikolay Kuchumov <kuchumovn@gmail.com>
2015-11-25 03:39:29 +0300

    html assets docs misc

--- code/client/html assets.js
+++ code/client/html assets.js
@@ -1,4 +1,13 @@
-// include these assets in webpack build
+// These are the assets used in "code/page-server/html.js" file
+// which is a React component used to render React on the server side.
+//
+// This file needs to be require()d from anywhere in your client code
+// in order for these assets to be detected and parsed by Webpack.
+//
+// Otherwise these assets won't be a part of Webpack build 
+// since "code/page server/html.js" is not require()d anywhere in client code
+// because it's only needed for server side rendering.
+//
 // (you'll also need to add the corresponding asset types to isomorphic.js;
 //  otherwise you'll get syntax errors when requiring these files on server)
 export default
