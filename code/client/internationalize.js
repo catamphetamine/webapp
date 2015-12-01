7e538ee0bad7416e24529dc3653228171d7e8f3a
Nikolay Kuchumov <kuchumovn@gmail.com>
2015-12-02 02:11:54 +0300

    misc

--- code/client/internationalize.js
+++ code/client/internationalize.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react'
 import hoist_statics from 'hoist-non-react-statics'
 import { injectIntl as international } from 'react-intl'
 
-/*
-  Note:
-    When this decorator is used, it MUST be the first (outermost) decorator.
-    Otherwise, we cannot find and call the preload and preload_deferred methods.
-*/
-
 export default function()
 {
 	return function(Wrapped)
