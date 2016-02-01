9889711b78027cacf72463d17fdaa56804a60c3f
kuchumovn@gmail.com <kuchumovn@gmail.com>
2016-02-01 03:05:12 +0300

    latest access time + online status store

--- code/authentication-service/api/authentication.js
+++ code/authentication-service/api/authentication.js
@@ -6,30 +6,383 @@
 import bcrypt from 'bcrypt'
 import jwt    from 'jsonwebtoken'
 import uid    from 'uid-safe'
+import moment from 'moment'
 
 Promise.promisifyAll(bcrypt)
 
-const users = new Map()
-let id_counter = 0
+import redis  from 'redis'
 
-function find_user_by_id(id)
+Promise.promisifyAll(redis)
+
+// user's latest activity time accuracy
+function round_user_access_time(time)
 {
-	return Promise.resolve(users.get(id))
+	return new Date(moment(time).seconds(0).unix() * 1000)
 }
 
-function find_user_by_email(email)
+function generate_jwt_id()
 {
-	for (let [user_id, user] of users)
+	return uid.sync(24)
+}
+
+api.post('/sign-in', async function({ email, password }, { ip, set_cookie, keys })
+{
+	const user = await store.find_user_by_email(email)
+
+	if (!user)
 	{
-		if (user.email === email)
-		{
-			return Promise.resolve(user)
-		}
+		throw new Errors.Not_found(`User with email ${email} not found`)
 	}
 
-	return Promise.resolve()
+	const matches = await check_password(password, user.password)
+
+	if (!matches)
+	{
+		throw new Errors.Generic(`Wrong password`) 
+	}
+
+	const jwt_id = store.generate_unique_jwt_id(user)
+
+	const token = jwt.sign({}, keys[0],
+	{
+		subject : user.id,
+		jwtid   : jwt_id
+	})
+
+	await store.add_authentication_token(user, jwt_id, ip)
+
+	// http://stackoverflow.com/questions/3290424/set-a-cookie-to-never-expire
+	const expires = new Date(2147483647000)  // January 2038
+	set_cookie('authentication', token, { expires, signed: false })
+
+	await store.update_user(user)
+
+	return { id: user.id, name: user.name }
+})
+
+api.post('/register', async function({ name, email, password })
+{
+	if (!exists(name))
+	{
+		throw new Errors.Input_missing(`"name" not specified`)
+	}
+
+	if (!exists(email))
+	{
+		throw new Errors.Input_missing(`"email" not specified`)
+	}
+
+	if (!exists(password))
+	{
+		throw new Errors.Input_missing(`"password" not specified`)
+	}
+
+	if (await store.find_user_by_email(email))
+	{
+		throw new Errors.Generic(`User with email ${email} already exists`)
+	}
+
+	password = await hash_password(password)
+
+	const id = store.create_user({ name, email, password })
+
+	return { id }
+})
+
+api.post('/authenticate', async function({}, { user, authentication_token_id, ip })
+{
+	// console.log('*** authenticate')
+
+	if (!user)
+	{
+		// throw authentication_error
+		return
+	}
+
+	user = await store.find_user_by_id(user.id)
+
+	if (!user)
+	{
+		return
+	}
+
+	// update this authentication token's last access IP and time
+	store.record_access(user, authentication_token_id, ip)
+
+	return { id: user.id, name: user.name }
+})
+
+api.post('/sign-out', async function({}, { destroy_cookie, user, authentication_token_id })
+{
+	if (!user)
+	{
+		return
+	}
+
+	// console.log('*** user before sign out', user)
+
+	await store.revoke_token(authentication_token_id, user.id)
+
+	// console.log('*** user after sign out', user)
+
+	destroy_cookie('authentication')
+})
+
+api.get('/validate-token', async function({}, { authentication_token_id, user })
+{
+	if (!user)
+	{
+		return { valid: false }
+	}
+
+	const token = await store.find_token_by_id(authentication_token_id, user.id)
+
+	if (!token)
+	{
+		return { valid: false }
+	}
+
+	return { valid: true }
+})
+
+api.post('/record-access', async function({}, { authentication_token_id, user, ip })
+{
+	if (!user)
+	{
+		return
+	}
+
+	await store.record_access(user, authentication_token_id, ip)
+})
+
+api.get('/latest-activity', async function({ user })
+{
+	user = store.find_user_by_id(user)
+
+	if (!user)
+	{
+		throw new Errors.Not_found(`User not found`)
+	}
+
+	return { time: user.latest_activity_time }
+
+	// const tokens = user.authentication_tokens || []
+
+	// return tokens.reduce((latest, token) =>
+	// {
+	// 	if (!latest || token.time > latest.time)
+	// 	{
+	// 		return token
+	// 	}
+	// })
+})
+
+function check_password(password, hashed_password)
+{
+	return bcrypt.compareAsync(password, hashed_password)
 }
 
+async function hash_password(password)
+{
+	const salt = await bcrypt.genSaltAsync(10)
+	return await bcrypt.hashAsync(password, salt)
+}
+
+class Memory_store
+{
+	connect()
+	{
+		this.users = new Map()
+		this.id_counter = 0
+	}
+
+	create_user(user)
+	{
+		user = Object.clone(user)
+
+		user.id = String(this.id_counter++)
+
+		this.users.set(user.id, user)
+	}
+
+	find_user_by_id(id)
+	{
+		return Promise.resolve(this.users.get(id))
+	}
+
+	find_user_by_email(email)
+	{
+		for (let [user_id, user] of this.users)
+		{
+			if (user.email === email)
+			{
+				return Promise.resolve(user)
+			}
+		}
+
+		return Promise.resolve()
+	}
+
+	update_user(user)
+	{
+		this.users.set(user.id, user)
+		return Promise.resolve()
+	}
+
+	generate_unique_jwt_id(user)
+	{
+		const token_id = generate_jwt_id()
+
+		if (user.authentication_tokens && user.authentication_tokens[token_id])
+		{
+			return this.generate_unique_jwt_id(user)
+		}
+
+		return token_id
+	}
+
+	find_token_by_id(token_id, user_id)
+	{
+		for (let [user_id, user] of this.users)
+		{
+			if (exists(user.authentication_tokens))
+			{
+				for (let token of Object.keys(user.authentication_tokens))
+				{
+					if (token === token_id)
+					{
+						return Promise.resolve(user.authentication_tokens[token])
+					}
+				}
+			}
+		}
+
+		return Promise.resolve()
+	}
+
+	async revoke_token(token_id, user_id)
+	{
+		const user = await this.find_user_by_id(user_id)
+		
+		// for in-memory database development testing
+		if (!user)
+		{
+			return
+		}
+
+		delete user.authentication_tokens[token_id]
+	}
+
+	add_authentication_token(user, jwt_id, ip)
+	{
+		user.authentication_tokens = user.authentication_tokens || []
+
+		const now = new Date()
+
+		user.authentication_tokens[jwt_id] = { ip, time: now }
+
+		// redundant field for faster latest activity time querying
+		user.latest_activity_time = now
+
+		return Promise.resolve()
+	}
+
+	async record_access(user, authentication_token_id, ip)
+	{
+		user = await store.find_user_by_id(user.id)
+
+		const now = round_user_access_time(new Date())
+
+		// update user's online status
+		let previous_time = await online_status_store.get_and_set(user.id, new Date())
+
+		if (previous_time)
+		{
+			previous_time = round_user_access_time(previous_time)
+		}
+
+		// console.log('*** previous user access time', previous_time)
+		// console.log('*** now user access time', now)
+
+		// if enough time has passed to update this user's latest activity time,
+		// then update it
+		if (!previous_time || now.getTime() > previous_time.getTime())
+		{
+			// console.log('*** updating user access time')
+
+			// if (previous_time)
+			// {
+			// 	console.log('*** previous_time.getTime()', previous_time.getTime())
+			// }
+
+			// update access time for this authentication token
+			const token = user.authentication_tokens[authentication_token_id]
+			token.ip = ip
+			token.time = now
+
+			// redundant field for faster latest activity time querying
+			user.latest_activity_time = now
+
+			await store.update_user(user)
+		}
+	}
+}
+
+class Mongodb_store
+{
+	connect()
+	{
+	}
+}
+
+class Memory_online_status_store
+{
+	connect()
+	{
+		this.user_sessions = {}
+	}
+
+	get_and_set(user_id, time)
+	{
+		const previous_time = this.user_sessions[user_id]
+		this.user_sessions[user_id] = time
+		return Promise.resolve(previous_time)
+	}
+}
+
+class Redis_online_status_store
+{
+	prefix = 'user:session:'
+	ttl = 10 * 60 // 10 minutes
+
+	connect()
+	{
+		this.client = redis.createClient
+		({
+			host      : configuration.redis.host,
+			port      : configuration.redis.port,
+			auth_pass : configuration.redis.password
+		})
+	}
+
+	get_and_set(user_id, time)
+	{
+		return this.client.multi()
+			.getset(this.prefix + user_id, time.toISOString())
+			.expire(this.prefix + user_id, this.ttl)
+			.execAsync()
+			.then(result => result[0] ? new Date(result[0]) : null)
+	}
+}
+
+const store = configuration.mongodb ? new Mongodb_store() : new Memory_store()
+store.connect()
+
+const online_status_store = configuration.redis ? new Redis_online_status_store() : new Memory_online_status_store()
+online_status_store.connect()
+
+// old session based code
+//
 // function find_user_by_remember_me_token(remember_me_token)
 // {
 // 	for (let [user_id, user] of users)
@@ -46,13 +399,7 @@ function find_user_by_email(email)
 // 		}
 // 	}
 // }
-
-function update_user(user)
-{
-	users.set(user.id, user)
-	return Promise.resolve()
-}
-
+//
 // function find_user_remember_me_token_by_session_id(user, session_id)
 // {
 // 	if (user.remember_me)
@@ -66,29 +413,12 @@ function update_user(user)
 // 		}
 // 	}
 // }
-
-function generate_unique_jwt_id(user)
-{
-	const token_id = generate_jwt_id()
-
-	if (user.authentication_tokens && user.authentication_tokens[token_id])
-	{
-		return generate_unique_jwt_id(user)
-	}
-
-	return token_id
-}
-
+//
 // function generate_remember_me_token()
 // {
 // 	return random_string(32, '#aA!')
 // }
-
-function generate_jwt_id()
-{
-	return uid.sync(24)
-}
-
+//
 // // http://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
 // function random_string(length, chars)
 // {
@@ -107,7 +437,7 @@ function generate_jwt_id()
 //
 // 	return result
 // }
-
+//
 // function user_signed_in(user, remember_me_token, ip, session, session_id, set_cookie)
 // {
 // 	// if we came here from `sign_in` method (not from `authenticate` method)
@@ -127,82 +457,6 @@ function generate_jwt_id()
 //
 // 	update_user(user)
 // }
-
-function find_token_by_id(token_id, user_id)
-{
-	for (let [user_id, user] of users)
-	{
-		if (exists(user.authentication_tokens))
-		{
-			for (let token of Object.keys(user.authentication_tokens))
-			{
-				if (token === token_id)
-				{
-					return Promise.resolve(user.authentication_tokens[token])
-				}
-			}
-		}
-	}
-
-	return Promise.resolve()
-}
-
-async function revoke_token(token_id, user_id)
-{
-	const user = await find_user_by_id(user_id)
-	
-	// for in-memory database development testing
-	if (!user)
-	{
-		return
-	}
-
-	delete user.authentication_tokens[token_id]
-}
-
-function add_authentication_token(user, jwt_id, ip)
-{
-	user.authentication_tokens = user.authentication_tokens || []
-
-	user.authentication_tokens[jwt_id] = { ip, time: new Date() }
-
-	return Promise.resolve()
-}
-
-api.post('/sign-in', async function({ email, password }, { ip, set_cookie, keys })
-{
-	const user = await find_user_by_email(email)
-
-	if (!user)
-	{
-		throw new Errors.Not_found(`User with email ${email} not found`)
-	}
-
-	const matches = await check_password(password, user.password)
-
-	if (!matches)
-	{
-		throw new Errors.Generic(`Wrong password`) 
-	}
-
-	const jwt_id = generate_unique_jwt_id(user)
-
-	const token = jwt.sign({}, keys[0],
-	{
-		subject : user.id,
-		jwtid   : jwt_id
-	})
-
-	await add_authentication_token(user, jwt_id, ip)
-
-	// http://stackoverflow.com/questions/3290424/set-a-cookie-to-never-expire
-	const expires = new Date(2147483647000)  // January 2038
-	set_cookie('authentication', token, { expires, signed: false })
-
-	await update_user(user)
-
-	return { id: user.id, name: user.name }
-})
 
 // (session based user authentication)
 //
@@ -227,38 +481,6 @@ api.post('/sign-in', async function({ email, password }, { ip, set_cookie, keys 
 // 	return session.user
 // })
 
-api.post('/register', async function({ name, email, password })
-{
-	if (!exists(name))
-	{
-		throw new Errors.Input_missing(`"name" not specified`)
-	}
-
-	if (!exists(email))
-	{
-		throw new Errors.Input_missing(`"email" not specified`)
-	}
-
-	if (!exists(password))
-	{
-		throw new Errors.Input_missing(`"password" not specified`)
-	}
-
-	if (await find_user_by_email(email))
-	{
-		throw new Errors.Generic(`User with email ${email} already exists`)
-	}
-
-	id_counter++
-	const id = String(id_counter)
-
-	password = await hash_password(password)
-
-	users.set(id, { id, name, email, password })
-
-	return { id }
-})
-
 // (session based user authentication)
 //
 // api.post('/authenticate', async function({}, { session, session_id, ip, get_cookie, set_cookie, destroy_cookie })
@@ -281,29 +503,6 @@ api.post('/register', async function({ name, email, password })
 //
 // 	return { id: session.user.id, name: session.user.name }
 // })
-
-api.post('/authenticate', async function({}, { user, authentication_token_id, ip })
-{
-	// console.log('*** authenticate')
-
-	if (!user)
-	{
-		// throw authentication_error
-		return
-	}
-
-	user = await find_user_by_id(user.id)
-
-	if (!user)
-	{
-		return
-	}
-
-	// update this authentication token's last access IP and time
-	record_access(user, authentication_token_id, ip)
-
-	return { id: user.id, name: user.name }
-})
 
 // (session based user authentication)
 //
@@ -332,68 +531,3 @@ api.post('/authenticate', async function({}, { user, authentication_token_id, ip
 //
 // 	destroy_session()
 // })
-
-api.post('/sign-out', async function({}, { destroy_cookie, user, authentication_token_id })
-{
-	if (!user)
-	{
-		return
-	}
-
-	// console.log('*** user before sign out', user)
-
-	await revoke_token(authentication_token_id, user.id)
-
-	// console.log('*** user after sign out', user)
-
-	destroy_cookie('authentication')
-})
-
-api.get('/validate-token', async function({}, { authentication_token_id, user })
-{
-	if (!user)
-	{
-		return { valid: false }
-	}
-
-	const token = await find_token_by_id(authentication_token_id, user.id)
-
-	if (!token)
-	{
-		return { valid: false }
-	}
-
-	return { valid: true }
-})
-
-api.post('/record-access', async function({}, { authentication_token_id, user, ip })
-{
-	if (!user)
-	{
-		return
-	}
-
-	await record_access(user, authentication_token_id, ip)
-})
-
-async function record_access(user, authentication_token_id, ip)
-{
-	user = await find_user_by_id(user.id)
-
-	const token = user.authentication_tokens[authentication_token_id]
-	token.ip = ip
-	token.time = new Date()
-
-	await update_user(user)
-}
-
-function check_password(password, hashed_password)
-{
-	return bcrypt.compareAsync(password, hashed_password)
-}
-
-async function hash_password(password)
-{
-	const salt = await bcrypt.genSaltAsync(10)
-	return await bcrypt.hashAsync(password, salt)
-}
